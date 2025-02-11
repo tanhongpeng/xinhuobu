78f678874bd1d987f51c970c1be9dd610f88b096
tanhongpeng <fenghounet@163.com>
2025-02-11 13:10:15 +0800

    更新代码功能

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 App({
-  globalData: {
-    isLaunched: false
-  },
   onLaunch() {
-    // 在launch页成功跳转后设置为true
+    wx.loadFontFace({
+      family: 'WenKai',
+      source: 'url("https://fonts.lxgw.io/css2?family=LXGWWenKai")',
+      success: () => console.log('全局字体预加载成功'),
+      fail: () => console.warn('网络字体预加载失败')
+    })
   }
 })
